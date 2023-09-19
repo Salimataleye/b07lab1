eb503538bcdc0eb2bfbd8e43cbdd555b2dde315c
Salimata Leye <salimataleye@MacBook-Air-de-Salimata.local>
2023-09-19 12:58:43 -0400

    Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -16,34 +16,48 @@ public class Polynomial {
 
 	public Polynomial add(Polynomial addedPoly) {
        
-		double[] newPoly = new double[addedPoly.poly.length];
-		for (int i = 0; i < addedPoly.poly.length; i++){
-			if(i < poly.length){
-				newPoly[i] = poly[i] + addedPoly.poly[i];
-			}
-			else{
+		int maxLength = Math.max(this.poly.length, addedPoly.poly.length);
+		double[] newPoly = new double[maxLength];
+		if (addedPoly.poly.length >= this.poly.length) {
+		    for (int i = 0; i < addedPoly.poly.length; i++){
+			   if(i < poly.length){
+				newPoly[i] = this.poly[i] + addedPoly.poly[i];
+			   }
+			   else{
 				newPoly[i] = addedPoly.poly[i];
-			}
+			   }
+		    }
+		}
+		else {
+			for (int i = 0; i < this.poly.length; i++){
+				   if(i < addedPoly.poly.length){
+					newPoly[i] = poly[i] + addedPoly.poly[i];
+				   }
+				   else{
+					newPoly[i] = this.poly[i];
+				   }
+			    }
+			
 		}
 		return new Polynomial(newPoly);
 	}
 
 	public double evaluate(double x) {
-		
+
 		double result = 0.0;
-		
+
 		for (int i = 0; i < poly.length; i++) {
 			result += (poly[i] * Math.pow(x, i));
 		}
-		return result;				
+		return result;
 	}
 
 	public boolean hasRoot(double x) {
-		
-		if (evaluate(x) == 0){
+
+		if (evaluate(x) == 0) {
 			return true;
 		}
-		return false;		
+		return false;
 	}
 
 }
