7b5c59bb91865303b5d27469528102a3a0a2bac6
Salimata Leye <salimataleye@MacBook-Air-de-Salimata.local>
2023-10-01 22:09:13 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,29 @@
+import java.io.IOException;
 public class Driver {
 	public static void main(String[] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double[] c1 = { 6, 0, 0, 5 };
-		Polynomial p1 = new Polynomial(c1);
-		double[] c2 = { 0, -2, 0, 0, -9 };
-		Polynomial p2 = new Polynomial(c2);
+		//Polynomial p = new Polynomial();
+		//System.out.println(p.evaluate(3));
+		double[] c1 = { 6, -2, 5 };
+		int[] e1 = {0, 1, 3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = {-6, 2, -5};
+		int[] e2 = {0, 1, 3};
+		Polynomial p2 = new Polynomial(c2, e2);
 		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if (s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
-	}
+		
+		try {
+			
+            p1.saveToFile("test_output.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+		//System.out.println(s.poly);
+		//System.out.println(s.exp);
+		 
+		
+		/*
+		 * if (s.hasRoot(1)) System.out.println("1 is a root of s"); else
+		 * System.out.println("1 is not a root of s");
+		 */
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,116 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 public class Polynomial {
 	double[] poly;
+	int[] exp;
 
 	public Polynomial() {
 		poly = new double[1];
+		exp = new int[1];
 		poly[0] = 0;
+		exp[0] = 0;
 	}
 
-	public Polynomial(double[] array) {
+	public Polynomial(double[] array, int[] powers) {
 
 		poly = new double[array.length];
+		exp = new int[powers.length];
 		for (int i = 0; i < array.length; i++) {
 			poly[i] = array[i];
 		}
+		for (int j = 0; j < powers.length; j++) {
+			exp[j] = powers[j];
+		}
+				
 	}
 
 	public Polynomial add(Polynomial addedPoly) {
 
 		int maxLength = Math.max(this.poly.length, addedPoly.poly.length);
-		double[] newPoly = new double[maxLength];
-		if (addedPoly.poly.length >= this.poly.length) {
-			for (int i = 0; i < addedPoly.poly.length; i++) {
-				if (i < poly.length) {
-					newPoly[i] = this.poly[i] + addedPoly.poly[i];
-				} else {
-					newPoly[i] = addedPoly.poly[i];
+		
+		ArrayList<Double> newPoly = new ArrayList<>();
+		ArrayList<Integer> newExp = new ArrayList<>();
+		int numzeros = 0;
+		
+		if(maxLength == addedPoly.poly.length) {
+			for(int i = 0; i < maxLength; i++) {
+				for(int j = 0; j < this.poly.length; j++) {
+					//In case there is a match
+					if(addedPoly.exp[i] == this.exp[j]) {
+						newPoly.add(addedPoly.poly[i]+this.poly[j]);
+						newExp.add(addedPoly.exp[i]);
+						if(addedPoly.poly[i]+this.poly[j] == 0.00) {
+							numzeros++;
+						}
+					}
+				}
+				if(!newExp.contains(addedPoly.exp[i])) {
+					newPoly.add(addedPoly.poly[i]);
+					newExp.add(addedPoly.exp[i]);
+				}
+								
+			}
+			
+			//if any leftovers in the smaller array
+			for(int i = 0; i < this.poly.length; i++) {
+				if(!newExp.contains(this.exp[i])) {
+					newPoly.add(this.poly[i]);
+					newExp.add(this.exp[i]);
 				}
 			}
-		} else {
-			for (int i = 0; i < this.poly.length; i++) {
-				if (i < addedPoly.poly.length) {
-					newPoly[i] = poly[i] + addedPoly.poly[i];
-				} else {
-					newPoly[i] = this.poly[i];
-				}
-			}
-
 		}
-		return new Polynomial(newPoly);
+		else {
+			for(int i = 0; i < maxLength; i++) {
+				for(int j = 0; j < addedPoly.poly.length; j++) {
+					if(this.exp[i] == addedPoly.exp[j]) {
+						newPoly.add(addedPoly.poly[j]+this.poly[i]);
+						newExp.add(this.exp[i]);
+						if(addedPoly.poly[j]+this.poly[i] == 0.00) {
+							numzeros++;
+						}
+					}
+				}
+				if(!newExp.contains(this.exp[i])) {
+					newPoly.add(this.poly[i]);
+					newExp.add(this.exp[i]);
+				}
+			}
+			for(int i = 0; i < addedPoly.poly.length; i++) {
+				if(!newExp.contains(addedPoly.exp[i])) {
+					newPoly.add(addedPoly.poly[i]);
+					newExp.add(addedPoly.exp[i]);
+				}
+			}
+		}
+		double[] coefficients = new double[newPoly.size() - numzeros];
+		int[] exponents = new int[newExp.size() - numzeros];
+		
+		int j =0;
+		for(int i = 0; i < newExp.size(); i++) {
+			if (newPoly.get(i) != 0) {
+				coefficients[j] = newPoly.get(i);
+				j++;
+						
+			}
+		}
+		j =0;
+		for(int i = 0; i < newPoly.size(); i++) {
+			if(newPoly.get(i) != 0) {
+				exponents[j] = newExp.get(i);
+				j++;
+		    }
+		}
+		
+		return new Polynomial(coefficients, exponents);	
+			
 	}
-
+		
+		
 	public double evaluate(double x) {
 
 		double result = 0.0;
@@ -56,5 +128,92 @@ public class Polynomial {
 		}
 		return false;
 	}
+	
+	public Polynomial multiply(Polynomial p2) {
+		
+		Polynomial finalPoly = new Polynomial();
+		
+		for (int i = 0; i < this.poly.length; i++ ) {
+	
+			for(int j = 0; j < p2.poly.length; j++){
+				
+				double coefficient = this.poly[i] * p2.poly[j];
+				int exponent = this.exp[i] + p2.exp[j];
+				finalPoly = finalPoly.add(new Polynomial(new double[] {coefficient}, new int[] {exponent}));
+			}			
+		}
+		   
+        return finalPoly;
+	}
+	
+	public Polynomial(File file) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
+
+        if (line == null) {
+            throw new IllegalArgumentException("Empty File");
+        }
+
+ 
+        String[] terms = line.split("[-+]");
+
+        double[] coefficients = new double[terms.length];
+        int[] exponents = new int[terms.length];
+
+        for (int i = 0; i < terms.length; i++) {
+            String term = terms[i];
+
+            String[] parts = term.split("x");
+            if (parts.length == 1) {
+            	
+                coefficients[i] = Double.parseDouble(parts[0]);
+                exponents[i] = 0;
+            } 
+            
+            else {
+            	
+                coefficients[i] = Double.parseDouble(parts[0]);
+                exponents[i] = Integer.parseInt(parts[1]);
+            }
+
+        
+            if (term.startsWith("-")) {
+                coefficients[i] *= -1;
+            }
+        }
+
+        poly = coefficients;
+        exp = exponents;
+        reader.close();
+    }
+	
+	public void saveToFile(String fileName) throws IOException {
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+		for (int i = 0; i < this.poly.length; i++) {
+			
+			double coefficient = this.poly[i];
+			int exponent = this.exp[i];
+
+			if (i != 0 && coefficient >= 0) {
+				writer.write("+");
+			}
+
+			if (coefficient != 0) {
+				writer.write(Double.toString(coefficient));
+
+				if (exponent != 0) {
+					
+					writer.write("x");
+					writer.write(Integer.toString(exponent));
+
+				}
+			}
+		}
+
+		writer.close();
+	}	
 
 }
